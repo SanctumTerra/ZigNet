a8fa753a256ff7ea413b92e4a5a3f9e561c5ba9c
Bonanoo <61703768+AnyBananaGAME@users.noreply.github.com>
2025-08-02 20:33:55 +0300

    Fix tiny memory leak

--- src/server/Server.zig
+++ src/server/Server.zig
@@ -145,6 +145,7 @@ pub const Server = struct {
                     Logger.ERROR("Failed to create unconnected pong: {s}", .{@errorName(err)});
                     return;
                 };
+                defer pong.deinit(allocator);
                 const pong_data = pong.serialize(self.options.allocator);
                 defer self.options.allocator.free(pong_data);
                 self.send(pong_data, from_addr);
