addff4200cc5ce77246604dce970f90a3787f18a
AnyBananaGAME <ainaraspirmas@gmail.com>
2025-09-14 17:15:01 +0300

    proto/offline/ConnectionReply2.zig: Better Error handling, Zig 0.15.1

--- src/proto/offline/ConnectionReply2.zig
+++ src/proto/offline/ConnectionReply2.zig
@@ -13,38 +13,29 @@ pub const ConnectionReply2 = struct {
         return .{ .guid = guid, .address = address, .mtu = mtu, .encryption_enabled = encryption_enabled };
     }
 
-    pub fn serialize(self: *ConnectionReply2, allocator: std.mem.Allocator) []const u8 {
+    pub fn serialize(self: *ConnectionReply2, allocator: std.mem.Allocator) ![]const u8 {
         const buffer = &[_]u8{};
         var stream = BinaryStream.init(allocator, buffer, 0);
         defer stream.deinit();
-        VarInt.write(&stream, Packets.OpenConnectionReply2);
-        Magic.write(&stream);
-        stream.writeInt64(self.guid, .Big);
-        const address_buffer = self.address.write(allocator) catch |err| {
-            Logger.ERROR("Failed to serialize address: {}", .{err});
-            return &[_]u8{};
-        };
-        stream.write(address_buffer);
+        try VarInt.write(&stream, Packets.OpenConnectionReply2);
+        try Magic.write(&stream);
+        try stream.writeInt64(self.guid, .Big);
+        const address_buffer = try self.address.write(allocator);
+        try stream.write(address_buffer);
         defer allocator.free(address_buffer);
-        stream.writeUint16(self.mtu, .Big);
-        stream.writeBool(self.encryption_enabled);
-        return stream.getBufferOwned(allocator) catch |err| {
-            Logger.ERROR("Failed to serialize connection reply 2: {}", .{err});
-            return &[_]u8{};
-        };
+        try stream.writeUint16(self.mtu, .Big);
+        try stream.writeBool(self.encryption_enabled);
+        return stream.getBufferOwned(allocator);
     }
 
-    pub fn deserialize(data: []const u8, allocator: std.mem.Allocator) ConnectionReply2 {
+    pub fn deserialize(data: []const u8, allocator: std.mem.Allocator) !ConnectionReply2 {
         var stream = BinaryStream.init(allocator, data, 0);
         defer stream.deinit();
-        _ = VarInt.read(&stream);
-        Magic.read(&stream);
-        const guid = stream.readInt64(.Big);
-        const address = Address.read(&stream, allocator) catch {
-            Logger.ERROR("Failed to deserialize address", .{});
-            return .{ .guid = 0, .address = Address.init(0, &[_]u8{}, 0), .mtu = 0, .encryption_enabled = false };
-        };
-        const mtu = stream.readUint16(.Big);
+        _ = try VarInt.read(&stream);
+        try Magic.read(&stream);
+        const guid = try stream.readInt64(.Big);
+        const address = try Address.read(&stream, allocator);
+        const mtu = try stream.readUint16(.Big);
         const encryption_enabled = stream.readBool();
         return .{ .guid = guid, .address = address, .mtu = mtu, .encryption_enabled = encryption_enabled };
     }
@@ -57,13 +48,13 @@ const VarInt = @import("BinaryStream").VarInt;
 
 test "ConnectionReply2" {
     const allocator = std.heap.page_allocator;
-    const test_address = Address.init(4, "192.168.1.1", 19132);
+    const test_address = Address.init(4, "1.1.1.1", 19132);
     var connection_reply2 = ConnectionReply2.init(987654321, test_address, 1492, false);
 
-    const serialized = connection_reply2.serialize(allocator);
+    const serialized = try connection_reply2.serialize(allocator);
     defer allocator.free(serialized);
 
-    var deserialized = ConnectionReply2.deserialize(serialized, allocator);
+    var deserialized = try ConnectionReply2.deserialize(serialized, allocator);
     defer deserialized.address.deinit(allocator);
 
     try std.testing.expectEqual(connection_reply2.guid, deserialized.guid);
