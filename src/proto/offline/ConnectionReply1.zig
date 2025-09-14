3b92331050f61727b41e004fe7543bfda6275f07
AnyBananaGAME <ainaraspirmas@gmail.com>
2025-09-14 17:14:52 +0300

    proto/offline/ConnectionReply1.zig: Better Error handling, Zig 0.15.1

--- src/proto/offline/ConnectionReply1.zig
+++ src/proto/offline/ConnectionReply1.zig
@@ -17,29 +17,26 @@ pub const ConnectionReply1 = struct {
         _ = allocator;
     }
 
-    pub fn serialize(self: *ConnectionReply1, allocator: std.mem.Allocator) []const u8 {
+    pub fn serialize(self: *ConnectionReply1, allocator: std.mem.Allocator) ![]const u8 {
         const buffer = &[_]u8{};
         var stream = BinaryStream.init(allocator, buffer, 0);
         defer stream.deinit();
-        VarInt.write(&stream, Packets.OpenConnectionReply1);
-        Magic.write(&stream);
-        stream.writeInt64(self.guid, .Big);
-        stream.writeBool(self.hasSecurity);
-        stream.writeUint16(self.mtu_size, .Big);
-        return stream.getBufferOwned(allocator) catch |err| {
-            Logger.ERROR("Failed to serialize connection reply 1: {}", .{err});
-            return &[_]u8{};
-        };
+        try VarInt.write(&stream, Packets.OpenConnectionReply1);
+        try Magic.write(&stream);
+        try stream.writeInt64(self.guid, .Big);
+        try stream.writeBool(self.hasSecurity);
+        try stream.writeUint16(self.mtu_size, .Big);
+        return stream.getBufferOwned(allocator);
     }
 
-    pub fn deserialize(data: []const u8, allocator: std.mem.Allocator) ConnectionReply1 {
+    pub fn deserialize(data: []const u8, allocator: std.mem.Allocator) !ConnectionReply1 {
         var stream = BinaryStream.init(allocator, data, 0);
         defer stream.deinit();
-        _ = VarInt.read(&stream);
-        Magic.read(&stream);
-        const guid = stream.readInt64(.Big);
-        const hasSecurity = stream.readBool();
-        const mtu_size = stream.readUint16(.Big);
+        _ = try VarInt.read(&stream);
+        try Magic.read(&stream);
+        const guid = try stream.readInt64(.Big);
+        const hasSecurity = try stream.readBool();
+        const mtu_size = try stream.readUint16(.Big);
         return .{ .guid = guid, .hasSecurity = hasSecurity, .mtu_size = mtu_size };
     }
 };
@@ -52,10 +49,10 @@ test "ConnectionReply1" {
     const allocator = std.heap.page_allocator;
     var connection_reply1 = ConnectionReply1.init(123456789, true, 1492);
 
-    const serialized = connection_reply1.serialize(allocator);
+    const serialized = try connection_reply1.serialize(allocator);
     defer allocator.free(serialized);
 
-    var deserialized = ConnectionReply1.deserialize(serialized, allocator);
+    var deserialized = try ConnectionReply1.deserialize(serialized, allocator);
     defer deserialized.deinit(allocator);
 
     try std.testing.expectEqual(connection_reply1.guid, deserialized.guid);
