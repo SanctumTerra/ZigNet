0785f3b6b44acd3619c49880756695b5badd4c0d
AnyBananaGAME <ainaraspirmas@gmail.com>
2025-09-14 17:15:20 +0300

    proto/offline/UnconnectedPing.zig: Better Error handling, Zig 0.15.1

--- src/proto/offline/UnconnectedPing.zig
+++ src/proto/offline/UnconnectedPing.zig
@@ -13,28 +13,25 @@ pub const UnconnectedPing = struct {
         self.guid = 0;
     }
 
-    pub fn serialize(self: *UnconnectedPing, allocator: std.mem.Allocator) []const u8 {
+    pub fn serialize(self: *UnconnectedPing, allocator: std.mem.Allocator) ![]const u8 {
         const buffer = &[_]u8{};
         var stream = BinaryStream.init(allocator, buffer, 0);
         defer stream.deinit();
-        VarInt.write(&stream, Packets.UnconnectedPing);
-        stream.writeInt64(self.timestamp, .Big);
-        Magic.write(&stream);
-        stream.writeInt64(self.guid, .Big);
-        const payload = stream.getBufferOwned(allocator) catch {
-            Logger.ERROR("Failed to serialize unconnected pong", .{});
-            return "";
-        }; // defer CAllocator.get().free(payload);
+        try VarInt.write(&stream, Packets.UnconnectedPing);
+        try stream.writeInt64(self.timestamp, .Big);
+        try Magic.write(&stream);
+        try stream.writeInt64(self.guid, .Big);
+        const payload = try stream.getBufferOwned(allocator);
         return payload;
     }
 
-    pub fn deserialize(data: []const u8, allocator: std.mem.Allocator) UnconnectedPing {
+    pub fn deserialize(data: []const u8, allocator: std.mem.Allocator) !UnconnectedPing {
         var stream = BinaryStream.init(allocator, data, 0);
         defer stream.deinit();
-        _ = VarInt.read(&stream);
-        const timestamp = stream.readInt64(.Big);
-        Magic.read(&stream);
-        const guid = stream.readInt64(.Big);
+        _ = try VarInt.read(&stream);
+        const timestamp = try stream.readInt64(.Big);
+        try Magic.read(&stream);
+        const guid = try stream.readInt64(.Big);
         return .{ .timestamp = timestamp, .guid = guid };
     }
 };
@@ -50,10 +47,10 @@ test "Unconnected Ping" {
     var ping = UnconnectedPing.init(123456789, 987654321);
     defer ping.deinit();
 
-    const serialized = ping.serialize(allocator);
+    const serialized = try ping.serialize(allocator);
     defer allocator.free(serialized);
 
-    var deserialized = UnconnectedPing.deserialize(serialized, allocator);
+    var deserialized = try UnconnectedPing.deserialize(serialized, allocator);
     defer deserialized.deinit();
 
     try std.testing.expectEqual(ping.timestamp, deserialized.timestamp);
